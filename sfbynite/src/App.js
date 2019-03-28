f92841ae18880472f689b5c85977a83010498f66
Marissa Conner <talk@fairgroundcreative.com>
2019-03-28 13:55:10 -0700

    Creation of site navigation component w react-boostrap

--- sfbynite/src/App.js
+++ sfbynite/src/App.js
@@ -1,89 +1,12 @@
 import React, { Component } from "react";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button
-} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import SiteNavigation from "./components/general/SiteNavigation.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#">
-                Modern Nights
-              </a>
-            </div>
-            <ul className="nav navbar-nav  mr-auto mt-2 mt-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Make A Character
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Policies
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Staff Training
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Help Files
-                </a>
-              </li>
-            </ul>
-            <ul className="nav navbar-right navbar-nav">
-              <li className="nav-item">
-                <button className="btn navbar-btn btn-light">Sign Up</button>
-              </li>
-              <li className="nav-item">
-                <span className="nav-link">Log In</span>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <SiteNavigation />
 
         <div className="container">
           <div id="splash">
