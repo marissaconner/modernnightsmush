0eb2c13b00a46821234104f2e0772e16454c3ca5
Marissa Conner <talk@fairgroundcreative.com>
2019-03-28 09:52:26 -0700

    ugly set up of basic homepage

--- sfbynite/src/App.js
+++ sfbynite/src/App.js
@@ -3,7 +3,106 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hi!</div>;
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">
+                Modern Nights
+              </a>
+            </div>
+            <ul className="nav navbar-nav  mr-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Make A Character
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Policies
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Staff Training
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Help Files
+                </a>
+              </li>
+            </ul>
+            <ul className="nav navbar-right navbar-nav">
+              <li className="nav-item">
+                <button className="btn navbar-btn btn-light">Sign Up</button>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link">Log In</span>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div id="splash">
+            <div className="row">
+              <h1>
+                Modern Nights
+                <small>A Vampire: The Masquerade Role Playing Game</small>
+              </h1>
+            </div>
+
+            <div className="row">
+              <div className="col-md-6">
+                <h2>Quick Reference</h2>
+                <ul>
+                  <li>
+                    <b>Setting:</b> Present-day San Francisco, California
+                  </li>
+                  <li>
+                    <b>Playable Characters:</b> Vampires (Camarilla, Anarch and
+                    Sabbat), plus revenants, ghouls, and humans (mundane or with
+                    numina).
+                  </li>
+                  <li>
+                    <b>Ruleset:</b> Vampire: The Masquerade 20th Anniversary
+                    Edition
+                    <p>
+                      We use the following supplemental v20 source books:
+                      <ul>
+                        <li>Ghouls And Revenants</li>
+                        <li>Lore Of The Clans</li>
+                        <li>Hunters Hunted II</li>
+                      </ul>
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-6">
+                <h2>Connect And Play</h2>
+                <p>
+                  Modern Nights is a MUSH (which is a text-based online gaming
+                  platform). We're working on making Modern Nights playable
+                  through the browser. In the meantime, you will need to
+                  download a client such as <a href="#">MushClient</a> (for
+                  Windows) or <a href="#">Atlantis</a> (for Mac).
+                </p>
+                <p>
+                  To play, connect to <b>www.BigDamnMush.com</b> port{" "}
+                  <b>1777</b>.
+                </p>
+                <p>
+                  Connection issues? Try connecting directly to{" "}
+                  <b>45.56.127.110</b> port <b>1777</b>.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
