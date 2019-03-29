09ce0d5cb3a6d3460109b0623ea8a18e7c325dd0
Marissa Conner <talk@fairgroundcreative.com>
2019-03-29 09:57:43 -0700

    Toggle-view on request reply form

--- sfbynite/src/components/jobs/OpenedRequest.js
+++ sfbynite/src/components/jobs/OpenedRequest.js
@@ -9,64 +9,58 @@ import {
 import RequestComment from "./RequestComment.js";
 
 const OpenedRequest = () => (
-  <div className="requests">
-    <div className="row">
-      <span className="request-originator">Jimbo</span>
-      <span className="request-header">Request Title</span>
-      <span className="request-date">May 23</span>
+  <div className="request">
+    <div className="request-header">
+      <span>back</span>
+      <h2>Request Title</h2>
+      <p>
+        <span class="request-originator">Player Name</span>
+        <Dropdown>
+          <Dropdown.Toggle size="sm">Player's Jobs</Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">Another Request Title</Dropdown.Item>
+            <Dropdown.Item href="#">Yet More Request Titles</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </p>
+
+      <p class="request-readerlist">To: Jimbo, Timmy, and Garth</p>
     </div>
+    <div className="request-body">
+      <p>Hello, blah blah blah, this is the body of my initial +request.</p>
 
-    <div className="request">
-      <div className="request-header">
-        <span>back</span>
-        <h2>Request Title</h2>
-        <p>
-          <span class="request-originator">Player Name</span>
-          <Dropdown>
-            <Dropdown.Toggle size="sm">Player's Jobs</Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item href="#">Another Request Title</Dropdown.Item>
-              <Dropdown.Item href="#">Yet More Request Titles</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </p>
+      <RequestComment
+        commentName="Timmy"
+        commentContents="This is my comment."
+        commentDate="March 23"
+      />
 
-        <p class="request-readerlist">To: Jimbo, Timmy, and Garth</p>
-      </div>
-      <div className="request-body">
-        <p>Hello, blah blah blah, this is the body of my initial +request.</p>
+      <Form>
+        <ButtonToolbar>
+          <Button data-toggle="collapse" href="#reply">
+            Reply
+          </Button>
+          <Button>Share</Button>
+          <Button>Assign</Button>
+        </ButtonToolbar>
 
-        <RequestComment
-          commentName="Timmy"
-          commentContents="This is my comment."
-          commentDate="March 23"
-        />
-        <Form>
-          <ButtonToolbar>
-            <Button data-toggle="collapse" href="#reply">
-              Reply
-            </Button>
-            <Button>Share</Button>
-            <Button>Assign</Button>
-          </ButtonToolbar>
-
-          <div id="reply" class="collapse">
-            <Form.Label>Reply</Form.Label>
-            <Form.Control as="textarea" rows="3" />
-
-            <Dropdown as={ButtonGroup}>
-              <Button>Add Reply</Button>
-              <Dropdown.Toggle split id="reply-variations" />
-              <Dropdown.Menu>
-                <Dropdown.Item eventKey="1">Approve</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Deny</Dropdown.Item>
-                <Dropdown.Item eventKey="3">Close</Dropdown.Item>
-                <Dropdown.Item eventKey="4">Reply</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+        <div id="reply" class="collapse">
+          <Form.Label>Comments</Form.Label>
+          <span data-toggle="collapse" href="#reply" class="close">
+            &times;
+          </span>
+          <Form.Control as="textarea" rows="3" />
+          <div id="job-reply-toolbar">
+            <Form.Control as="select">
+              <option>Comment</option>
+              <option>Approve</option>
+              <option>Close</option>
+              <option>Deny</option>
+            </Form.Control>
+            <Button>Send</Button>
           </div>
-        </Form>
-      </div>
+        </div>
+      </Form>
     </div>
   </div>
 );
