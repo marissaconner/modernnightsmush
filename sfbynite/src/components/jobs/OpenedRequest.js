be92de1034a4a4580eaf2782024bbc559d1d4929
Marissa Conner <talk@fairgroundcreative.com>
2019-03-30 10:49:06 -0700

    Togglable 'share with' to add other users to a job

--- sfbynite/src/components/jobs/OpenedRequest.js
+++ sfbynite/src/components/jobs/OpenedRequest.js
@@ -17,7 +17,7 @@ const OpenedRequest = () => (
       <p>
         <span className="request-originator">From: </span>
         <Dropdown className="narrow-inline-dropdown">
-          <Dropdown.Toggle size="sm">Player Name</Dropdown.Toggle>
+          <Dropdown.Toggle size="sm">Player name </Dropdown.Toggle>
           <Dropdown.Menu>
             <Dropdown.Item href="#">Another Request Title</Dropdown.Item>
             <Dropdown.Item href="#">Yet More Request Titles</Dropdown.Item>
@@ -41,43 +41,77 @@ const OpenedRequest = () => (
       />
 
       <Form>
-        <Button>Share</Button>
+        <span
+          className="btn btn-default"
+          data-toggle="collapse"
+          data-target="#share-with"
+        >
+          Share
+        </span>
 
-        {/*  Using react-bootstrap here because I will want animations/transitions and so on so I will need to inevitably convert it. */}
-        <Form.Group controlId="share-with">
-          <Form.Label>Share With:</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
+        <span
+          className="btn btn-primary"
+          data-toggle="collapse"
+          data-target="#user-reply"
+        >
+          Reply
+        </span>
 
-        <div className="form-group">
-          <label for="reply">Reply</label>
-          <textarea name="reply" className="form-control" id="reply" />
+        <div className="form-group collapse" id="share-with">
+          <label for="cc-list">Share With...</label>
+          <span
+            className="close"
+            data-target="#share-with"
+            data-toggle="collapse"
+          >
+            &times;
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            name="cc-list"
+            id="cc-list"
+          />
         </div>
 
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary">
-            Send
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary dropdown-toggle dropdown-toggle-split"
-            data-toggle="dropdown"
-          >
-            <span class="caret" />
-          </button>
-          <div class="dropdown-menu">
-            <a href="#" class="dropdown-item">
+        <div id="user-reply" className="collapse">
+          <div className="form-group">
+            <label for="reply">Reply</label>
+            <span
+              className="close"
+              data-target="#user-reply"
+              data-toggle="collapse"
+            >
+              &times;
+            </span>
+            <textarea name="reply" className="form-control" id="reply" />
+          </div>
+
+          <div class="btn-group">
+            <button type="button" class="btn btn-primary">
               Send
-            </a>
-            <a href="#" class="dropdown-item">
-              Send & Approve
-            </a>
-            <a href="#" class="dropdown-item">
-              Send & Close
-            </a>
-            <a href="#" class="dropdown-item">
-              Send & Deny
-            </a>
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary dropdown-toggle dropdown-toggle-split"
+              data-toggle="dropdown"
+            >
+              <span class="caret" />
+            </button>
+            <div class="dropdown-menu">
+              <a href="#" class="dropdown-item">
+                Send
+              </a>
+              <a href="#" class="dropdown-item">
+                Send & Approve
+              </a>
+              <a href="#" class="dropdown-item">
+                Send & Close
+              </a>
+              <a href="#" class="dropdown-item">
+                Send & Deny
+              </a>
+            </div>
           </div>
         </div>
       </Form>
